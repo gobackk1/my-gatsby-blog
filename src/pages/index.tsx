3cc391d40f01a24c4c7734e3357eb20a8facf2be
gobackk1 <k.kwbt@icloud.com>
2021-06-20 17:01:45 +0900

    [修正]updateAtを降順でクエリする

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import css from "@emotion/css"
 export default () => {
   const data: Props = useStaticQuery(graphql`
     {
-      allContentfulBlogPost {
+      allContentfulBlogPost(sort: { fields: updatedAt, order: DESC }) {
         edges {
           node {
             title
